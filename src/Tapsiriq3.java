36137ad2083a78630814a4a42b7855af5c8c6267
LeylaKazimli <kazimilleyla@gmail.com>
2026-07-03 00:51:36 +0400

    Tapsiriq3

---
+++ src/Tapsiriq3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Tapsiriq3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("3 reqemli ededi daxil edin: ");
+        int sayi = sc.nextInt();
+
+        int yuzler = sayi / 100;
+        int onlar = (sayi % 100) / 10;
+        int birler = sayi % 10;
+
+        int cem = yuzler + onlar + birler;
+
+        System.out.println("Yuzler: " + yuzler);
+        System.out.println("Onlar: " + onlar);
+        System.out.println("Birler: " + birler);
+        System.out.println("Reqemlerin cemi: " + cem);
+    }
+}
