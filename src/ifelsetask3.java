5f19ea26146b8f22b5a197ba3377a66755b196fa
LeylaKazimli <kazimilleyla@gmail.com>
2026-07-05 16:41:07 +0400

    ifelsetask3

---
+++ src/ifelsetask3.java
@@ -0,0 +1,26 @@
+// İstifadəçidən imtahan balını alır və bala uyğun qiyməti çap edir.
+
+import java.util.Scanner;
+
+    public class ifelsetask3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("İmtahan balını daxil edin: ");
+        int bal = sc.nextInt();
+
+        if (bal > 100 || bal < 0) {
+            System.out.println("Yanlış bal!");
+        } else if (bal >= 90) {
+            System.out.println("Əla (5)");
+        } else if (bal >= 80) {
+            System.out.println("Çox yaxşı (4)");
+        } else if (bal >= 70) {
+            System.out.println("Yaxşı (3)");
+        } else if (bal >= 60) {
+            System.out.println("Kafi (2)");
+        } else {
+            System.out.println("Qeyri-kafi (1)");
+        }
+    }
+}
