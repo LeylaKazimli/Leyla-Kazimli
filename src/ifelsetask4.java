29e000d13b07afeef148602e6b58f7dced4e4377
LeylaKazimli <kazimilleyla@gmail.com>
2026-07-05 16:50:20 +0400

    ifelsetask4

---
+++ src/ifelsetask4.java
@@ -0,0 +1,15 @@
+// İstifadəçidən il alır və uyğun (keçən) il olub-olmadığını yoxlayır.
+
+import java.util.Scanner;
+
+    public class ifelsetask4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("İli daxil edin: ");
+        int il = sc.nextInt();
+
+        System.out.println((il % 400 == 0 || il % 4 == 0 && il % 100 != 0) ? "Uyğun il" : "Uyğun deyil");
+    }
+}
