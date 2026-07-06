63255ce2b86ec456e771619f3af796cfb663b80b
LeylaKazimli <kazimilleyla@gmail.com>
2026-07-06 14:37:35 +0400

    ifelsetask6

---
+++ src/ifelsetask6.java
@@ -0,0 +1,44 @@
+
+     import java.util.Scanner;
+
+    public class ifelsetask6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Birinci ededi daxil edin: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Ikinci ededi daxil edin: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Emeliyyat isaresini daxil edin (+, -, *, /): ");
+        String operation = sc.next();
+
+        switch (operation) {
+
+            case "+":
+                System.out.println("Netice: " + (num1 + num2));
+                break;
+
+            case "-":
+                System.out.println("Netice: " + (num1 - num2));
+                break;
+
+            case "*":
+                System.out.println("Netice: " + (num1 * num2));
+                break;
+
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("0-a bolmek olmaz!");
+                } else {
+                    System.out.println("Netice: " + (num1 / num2));
+                }
+                break;
+
+            default:
+                System.out.println("Namelum emeliyyat!");
+        }
+    }
+}
