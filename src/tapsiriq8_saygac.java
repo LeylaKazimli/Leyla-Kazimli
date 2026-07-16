ba5dec694c701199c988d4e5580dfcfda864b550
LeylaKazimli <kazimilleyla@gmail.com>
2026-07-16 20:26:37 +0400

    tapsiriq8_saygac

---
+++ src/tapsiriq8_saygac.java
@@ -0,0 +1,17 @@
+public class tapsiriq8_saygac {
+    public static void main(String[] args) {
+
+        // Tapşırıq:
+        // do-while dövrəsindən istifadə edərək
+        // 1-dən 10-a qədər olan ədədləri
+        // hər birini yeni sətirdə ekrana çap etmək.
+
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+
+    }
+}
