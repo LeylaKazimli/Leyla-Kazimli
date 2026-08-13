84dd4a5069777423544b179feeba85b1dffa7657
LeylaKazimli <kazimilleyla@gmail.com>
2026-08-14 00:02:11 +0400

    textBox

--- src/LimitKecildiException.java
+++
@@ -1,7 +0,0 @@
-public class LimitKecildiException extends Exception {
-
-    public LimitKecildiException(String message) {
-        super(message);
-    }
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,7 @@
 public class Main {
-
-    public static void nagdCixar(double meblegh) throws LimitKecildiException {
-
-        if (meblegh > 2000) {
-            throw new LimitKecildiException("Gündəlik limit (2000 AZN) keçildi.");
-        }
-
-        System.out.println("Nağd çıxarıldı: " + meblegh + " AZN");
-    }
-
     public static void main(String[] args) {
 
-        try {
-            nagdCixar(1500);
-            nagdCixar(2500);
-        }
-        catch (LimitKecildiException e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println();
 
     }
 }
